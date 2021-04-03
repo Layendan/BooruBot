39921b012fd0701d0e433538f8a6f52b3727324d
Layendan <layendan.h@gmail.com>
2021-04-03 02:21:24 -0700

    slash my life away

--- discord_bot.js
+++ discord_bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN || '' /*change 'Your Discord Token Here to the token of your bot*/ //Reminder to change this to env var
+const TOKEN = 'ODI3NTkxNzY3NTkwMzA1ODMz.YGdQ5Q.HuvGQspSvEJT_O5PreYRXwR9xo8' || '' /*change 'Your Discord Token Here to the token of your bot*/ //Reminder to change this to env var
 const apiEndpoint = 'https://discord.com/api/v8/applications/827591767590305833/guilds/716516840146468925/commands'
 const Danbooru = require('danbooru')
 const login = ''
@@ -16,9 +16,9 @@ const key = ''
 
 //Later on we make functions when we understand the wrapper????
 
-const registerCommand = (data) => {
+/*const registerCommand = (data) => {
     bot.api
-        .applications(process.env.CLIENT_ID || bot.user.id)
+        .applications(process.env.CLIENT_ID)
         .commands.post({ data });
 };
 
@@ -33,45 +33,7 @@ registerCommand({
             required: false,
         },
     ],
-});
-
-bot.ws.on('INTERACTION_CREATE', async interaction => {
-    const booru = new Danbooru()
-    embed = ''
-    image = ''
-    await booru.posts({ tags: 'genshin_impact rating:explicit ' }).then(posts => { //Limit  does nothing?
-        // Select a random post from posts array
-        const index = Math.floor(Math.random() * posts.length)
-        console.log(posts.length)
-
-        console.log(index)
-
-        const post = posts[index]
-
-
-        // Get post's url
-        const url = booru.url(post.file_url)
-
-        const name = post.id
-
-        embed = new Discord.MessageEmbed()
-            .setColor('#00FFFF') //aqua
-            .setTitle(name) //index
-            .setURL('https://danbooru.donmai.us/posts/' + name) //links to page
-            .setImage(url.href)
-
-        image = url.href
-
-    })
-    bot.api.interactions(interaction.id, interaction.token).callback.post({
-        data: {
-            type: 3,
-            data: {
-                content: image
-            }
-        }
-    })
-})
+});*/
 
 bot.on('message', async function(message) {
 
@@ -165,6 +127,112 @@ bot.on('message', async function(message) {
 
     }
 
+});
+
+bot.ws.on("INTERACTION_CREATE", async (interaction) => {
+    const interact = (data) =>
+        bot.api.interactions(interaction.id, interaction.token).callback.post({
+            data,
+        });
+
+    try {
+        switch (interaction.data.name) {
+            case "hentai":
+                if (!interaction.data.options) {
+
+                    // Perform a search for popular image posts
+                    const booru = new Danbooru()
+                    booru.posts({ tags: 'genshin_impact rating:explicit ', limit: 30 }).then(posts => {
+                        // Select a random post from posts array
+                        const index = Math.floor(Math.random() * posts.length)
+                        console.log(posts.length)
+
+                        console.log(index)
+
+                        const post = posts[index]
+
+
+                        // Get post's url
+                        const url = booru.url(post.file_url)
+
+                        const name = post.id
+
+                        interact({
+                            type: 4,
+                            data: {
+                                content: "",
+                                embeds: [
+                                    {
+                                        title: name,
+                                        url: "https://danbooru.donmai.us/posts/" + name,
+                                        color: Discord.Constants.Colors.BLURPLE,
+                                        image: {
+                                            url: url.href,
+                                        },
+                                    },
+                                ],
+                            },
+                        });
+
+                    });
+                } else {
+                    // Perform a search for popular image posts
+                    const booru = new Danbooru()
+
+
+                    const post = await booru.posts(interaction.data.options.find(
+                                                  (option) => option.name == "id"
+                                                  ) &&
+                                                  interaction.data.options.find(
+                                                  (option) => option.name == "id"
+                                                  ).value);
+
+
+                        // Get post's url
+                        const url = booru.url(post.file_url)
+
+                        const name = post.id
+
+                        interact({
+                            type: 4,
+                            data: {
+                                content: "",
+                                embeds: [
+                                    {
+                                        title: name,
+                                        url: "https://danbooru.donmai.us/posts/" + name,
+                                        color: Discord.Constants.Colors.BLURPLE,
+                                        image: {
+                                            url: url.href,
+                                        },
+                                    },
+                                ],
+                            },
+                        });
+                }
+                break;
+        }
+
+    } catch (error) {
+
+        interact({
+            type: 4,
+            data: {
+                content: "",
+                embeds: [
+                    {
+                        title: "An error has occured!",
+                        description:
+                            "We're so sorry for the inconvenience!\n\nThe error has been automatically logged, so we'll start working on fixing this as soon as we see the message.\n\n" + error,
+                        thumbnail: {
+                            url: "https://i.imgur.com/J4jZEVD.png",
+                        },
+                        color: Discord.Constants.Colors.RED,
+                    },
+                ],
+            },
+        });
+    }
 });
 
 
