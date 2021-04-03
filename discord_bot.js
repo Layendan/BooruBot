e5f6ebae6a8c453ca1795bac5b4443ecc3759c35
Shezan Shiraz <shezanshiraz@gmail.com>
2021-04-03 13:07:35 +0530

    Added env Vars and prefix commands for bot

--- discord_bot.js
+++ discord_bot.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN || '' /*change 'Your Discord Token Here to the token of your bot*/ //Reminder to change this to env var
+const TOKEN = process.env.TOKEN /*change 'Your Discord Token Here to the token of your bot*/ //Reminder to change this to env var
 const apiEndpoint = 'https://discord.com/api/v8/applications/827591767590305833/guilds/716516840146468925/commands'
 const Danbooru = require('danbooru')
 const login = ''
 const key = ''
+const prefix = 'b!'
+
+
+
 
 
 
@@ -13,65 +18,62 @@ const key = ''
 /*anything with quotes ' ' information goes inside them*/
 
 
+// Later on we make functions when we understand the wrapper????
 
-//Later on we make functions when we understand the wrapper????
+// const registerCommand = (data) => {
+//     bot.api
+//         .applications(process.env.CLIENT_ID || bot.user.id)
+//         .commands.post({ data });
+// };
 
-const registerCommand = (data) => {
-    bot.api
-        .applications(process.env.CLIENT_ID || bot.user.id)
-        .commands.post({ data });
-};
+// registerCommand({
+//     name: "hentai",
+//     description: "Danbooru",
+//     options: [{
+//         name: "id",
+//         description: "The identifier of the post",
+//         type: 4,
+//         required: false,
+//     }, ],
+// });
 
-registerCommand({
-    name: "hentai",
-    description: "Danbooru",
-    options: [
-        {
-            name: "id",
-            description: "The identifier of the post",
-            type: 4,
-            required: false,
-        },
-    ],
-});
+// bot.ws.on('INTERACTION_CREATE', async interaction => {
+//     const booru = new Danbooru()
+//     embed = ''
+//     image = ''
+//     await booru.posts({ tags: 'genshin_impact rating:explicit ' }).then(posts => { //Limit  does nothing?
+//         // Select a random post from posts array
+//         const index = Math.floor(Math.random() * posts.length)
+//         console.log(posts.length)
 
-bot.ws.on('INTERACTION_CREATE', async interaction => {
-    const booru = new Danbooru()
-    embed = ''
-    image = ''
-    await booru.posts({ tags: 'genshin_impact rating:explicit ' }).then(posts => { //Limit  does nothing?
-        // Select a random post from posts array
-        const index = Math.floor(Math.random() * posts.length)
-        console.log(posts.length)
+//         console.log(index)
 
-        console.log(index)
-
-        const post = posts[index]
+//         const post = posts[index]
 
 
-        // Get post's url
-        const url = booru.url(post.file_url)
+//         // Get post's url
+//         const url = booru.url(post.file_url)
 
-        const name = post.id
+//         const name = post.id
 
-        embed = new Discord.MessageEmbed()
-            .setColor('#00FFFF') //aqua
-            .setTitle(name) //index
-            .setURL('https://danbooru.donmai.us/posts/' + name) //links to page
-            .setImage(url.href)
+//         embed = new Discord.MessageEmbed()
+//             .setColor('#00FFFF') //aqua
+//             .setTitle(name) //index
+//             .setURL('https://danbooru.donmai.us/posts/' + name) //links to page
+//             .setImage(url.href)
 
-        image = url.href
+//         image = url.href
 
-    })
-    bot.api.interactions(interaction.id, interaction.token).callback.post({
-        data: {
-            type: 3,
-            data: {
-                content: image
-            }
-        }
-    })
-})
+//     })
+//     bot.api.interactions(interaction.id, interaction.token).callback.post({
+//         data: {
+//             type: 3,
+//             data: {
+//                 content: image
+//             }
+//         }
+//     })
+// })
 
 bot.on('message', async function(message) {
 
@@ -82,12 +84,12 @@ bot.on('message', async function(message) {
         // Perform a search for popular image posts
 
         const booru = new Danbooru()
-        booru.posts({ tags: 'genshin_impact rating:explicit ', limit: 30 }).then(posts => { //Limit  does nothing?
+        booru.posts({ tags: 'genshin_impact rating:explicit ', limit: 100 }).then(posts => { //Limit  does nothing?
             // Select a random post from posts array
             const index = Math.floor(Math.random() * posts.length)
-            console.log(posts.length)
+            console.log("Found " + posts.length + " posts")
 
-            console.log(index)
+            console.log("Selected " + index)
 
             const post = posts[index]
 
@@ -167,6 +169,68 @@ bot.on('message', async function(message) {
 
 });
 
+
+bot.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'tags') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+
+        if (args.length > 3) {
+            return message.channel.send(`You can only input 3 tags, ${message.author}`) //Currently unauthed users can only query 3 tags
+        }
+
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+        console.log(`User ${message.member.user.tag} invoked the ${command} command`)
+
+        // Perform a search for popular image posts
+
+        const booru = new Danbooru()
+
+
+        tags = args.join(' ') //Joining the tags with a space to search with later 
+
+        console.log(tags)
+        booru.posts({ tags: tags, limit: 100 }).then(posts => { //Limit  does nothing?
+            // Select a random post from posts array
+
+            const index = Math.floor(Math.random() * posts.length)
+            console.log("Found " + posts.length + " posts")
+
+            if (posts.length === 0) { //If posts length is 0 that means the tags used to search are invalid
+                return message.channel.send(`No posts found, invalid tags used, ${message.author}.`)
+
+            }
+
+            console.log("Selected Post index is " + index)
+
+            const post = posts[index]
+
+
+            // Get post's url
+            const url = booru.url(post.file_url)
+
+            const name = post.id
+
+            const embed = new Discord.MessageEmbed()
+                .setColor('#00FFFF') //aqua
+                .setTitle(name) //index
+                .setURL('https://danbooru.donmai.us/posts/' + name) //links to page
+                .setImage(url.href)
+                .setFooter(message.member.user.tag, message.member.user.avatarURL()) //member name + member image
+
+            message.channel.send(embed)
+
+        })
+
+    }
+
+});
 
 
 bot.login(TOKEN);
